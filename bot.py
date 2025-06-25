f506cdf0849235e8766bc631c590c43e58be5adf
Iseyg91 <gokalpahmet74@gmail.com>
2025-06-25 21:36:33 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import discord
+import audioop
 from discord.ext import commands, tasks
 from discord import app_commands, Embed, ButtonStyle, ui
 from discord.ui import Button, View, Select, Modal, TextInput, button
