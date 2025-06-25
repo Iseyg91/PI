7ed145fb6c9672d548d6c2fe7c3968d2d0570669
Iseyg91 <gokalpahmet74@gmail.com>
2025-06-25 21:06:02 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,12 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()  # Pas besoin d'intents spéciaux si le bot ne fait rien
+bot = commands.Bot(command_prefix='?', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Bot connecté en tant que {bot.user}")
+
+# Remplace "TON_TOKEN_ICI" par le token réel de ton bot
+bot.run("TON_TOKEN_ICI")
